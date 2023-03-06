55e17356ff9025c8a5d59d4444a7e927d6e4a02f
BIBruington <psychological_chemist@hotmail.com>
2023-03-06 16:57:30 -0500

    add note

--- components/CardBuilder.jsx
+++ components/CardBuilder.jsx
@@ -1,18 +1,7 @@
-import { useState } from "react";
-import ReactMarkdown from "react-markdown"
+import { useState, useEffect } from "react";
+import uuid from "react-uuid"
 import { collection, addDoc } from "firebase/firestore"; 
 import { db } from "@/utils/firebase";
-
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
 
 export default function CardBuilder() {
 
@@ -20,18 +9,25 @@ export default function CardBuilder() {
   const [currentCard, setCurrentCard] = useState({
     question: null,
     answer: null,
+    id: null,
   });
   const [cardSet, setCardSet] = useState([]);
 
-  const addNotesToDb = async () => {
-    const docRef = await addDoc(collection(db, "topics", topic), )
-  }
-
-  const addNote = () => {
-    cardSet.push(currentCard);
+  const addNote = async () => {
+    const docRef = await addDoc(collection(db, "topics", topic, "notecards"), {
+      question: currentCard.question,
+      answer: currentCard.answer,
+      id: uuid(),
+      lastModified:{
+        seconds: Date.now()/1000,
+        milliseconds: Date.now()
+      }
+    })
+    cardSet.push(currentCard)
     setCurrentCard({
       question: null,
       answer: null,
+      id: null
     })
   }
 
@@ -39,29 +35,30 @@ export default function CardBuilder() {
     setCurrentCard({
       question: event.target.value,
       answer: currentCard.answer,  
+      id: currentCard.id? currentCard.id : uuid(),
   })}
 
   const editAnswer = (event) => {
     setCurrentCard({
       question: currentCard.question,
       answer: event.target.value,  
+      id: currentCard.id? currentCard.id : uuid(),
   })}
 
   return (
-    <div className="flex w-full">
+    <div className="flex flex-col w-1/2 md:flex-row md:w-full">
 
       {/* Note Card Builder */}
-      <button onClick={addNotesToDb}>Submit Questions</button>
       <main className="flex flex-col items-center w-full">
-        <div className="flex flex-col mt-10 w-4/6 items-center">
-          <span className="text-sm my-2">Insert your question:</span>
+        <div className="flex flex-col mt-10 w-full md:w-4/6 items-center">
+          <span className="text-sm mb-5 bg-black text-white py-2 w-full text-center rounded-sm">Insert your question:</span>
           <textarea id="question"
             onChange={editQuestion}
             className="flex mb-4 p-2 w-full h-30vh border border-black" type="text">
           </textarea>
         </div>
-        <div className="flex flex-col mt-10 w-4/6 items-center">
-          <span className="text-sm my-2">Insert your answer:</span>
+        <div className="flex flex-col mt-10 w-full md:w-4/6 items-center">
+          <span className="text-sm my-5 bg-black text-white py-2 w-full text-center rounded-sm">Insert your answer:</span>
           <textarea 
             onChange={editAnswer}
             className="flex mb-4 p-2 w-full h-80 border border-black" type="text"></textarea>
@@ -71,17 +68,20 @@ export default function CardBuilder() {
 
       {/* display note */}
 
-      <div className="flex flex-col">
-        <button onClick={addNote}>Add your notecard</button>
-        <span>Here is your note card</span>
-        <span>{currentCard.question} current card here</span>
-        <span>{currentCard.answer} current card here</span>
-        {cardSet.length > 0  && cardSet.map((card) => (
-          <div className="flex flex-col" key={card.question}>
-            <span>card set question: {card.question}</span>
-            <span>card set answer: {card.answer}</span>
-          </div>
-        ))}
+      <div className="flex flex-col mt-10 md:mr-10 md:w-2/6">
+        <button className="mb-5 bg-black text-white py-3 px-5 rounded-md hover:bg-gray-700 whitespace-nowrap w-auto"
+          onClick={addNote}>Add your notecard
+        </button>
+        <span className="mb-5 text-center">Here are your note cards</span>
+        <span>{currentCard.question}</span>
+        <span>{currentCard.answer}</span>
+        <div className="flex items-center justify-center">
+          { cardSet.length > 0  && cardSet.map((card, ind) => (
+            <div className="flex items-center" key={card.id}>
+              <button className="flex mr-2 text-center">{ind + 1}</button>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
