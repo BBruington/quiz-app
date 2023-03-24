6cc61b15c9dda044f5ffc07f23cf234b14c71b6a
BIBruington <psychological_chemist@hotmail.com>
2023-03-23 20:39:25 -0400

    testcard state

--- components/TestCard.jsx
+++ components/TestCard.jsx
@@ -1,20 +1,53 @@
-
+import { useState, useEffect } from "react";
 
 export default function TestCard() {
 
+  const [end, setEnd] = useState(false)
+  const [questionNum, setQuestionNum] = useState(0)
+  const [score, setScore] = useState(0)
+  const [testQuestions, setTestQuestions] = useState([]);
+  const [currentTestQuestions, setCurrentTestQuestions] = useState({
+    question: "",
+    answerOne: "",
+    answerTwo: "",
+    answerThree: "",
+    answerFour: "",
+    correct: null,
+  });
+
+  const answer = (guess) => {
+    if(currentTestQuestions.correct === guess) setScore(score + 1);
+    if(questionNum < testQuestions.length){
+      setQuestionNum(questionNum + 1);
+    } else{
+      setEnd(true);
+    }
+    
+  }
+
   return (
     <>
-      <div className="flex w-10/12">
-        <div className="flex items-center justify-center w-full bg-gray-200 h-80 my-20">This is where the question would be asked?</div>
-      </div>
-      <div className="flex flex-col md:flex-row items-center justify-around my-5">
-        <button className="h-28 w-4/6 md:w-5/12 bg-gray-200">answer 1</button>
-        <button className="h-28 w-4/6 md:w-5/12 bg-gray-200 my-10">answer 2</button>
-      </div>
-      <div className="flex flex-col md:flex-row items-center justify-around my-5">
-        <button className="h-28 w-4/6 md:w-5/12 bg-gray-200">answer 1</button>
-        <button className="h-28 w-4/6 md:w-5/12 bg-gray-200 my-10">answer 2</button>
-      </div>
+      {end ? (
+        <div>You got {score} out of {testQuestions.length} correct!</div>
+      )  :  (
+        <>
+          <div className="flex justify-between mt-10 mx-10">
+            <div>Your Current Score: {score}</div>
+            <div>Question {questionNum} of {testQuestions.length}</div>
+          </div>
+          <div className="flex items-center justify-center bg-gray-200 h-80 my-20 w-10/12 mx-auto">This is where the question would be asked?</div>      
+          
+          {/* answers: */}
+          <div className="flex flex-col md:flex-row items-center justify-around my-5">
+            <button className="testButton" onClick={() => answer(1)}>answer 1</button>
+            <button className="testButton mt-5 md:mt-0" onClick={() => answer(2)}>answer 2</button>
+          </div>
+          <div className="flex flex-col md:flex-row items-center justify-around my-5">
+            <button className="testButton" onClick={() => answer(3)}>answer 3</button>
+            <button className="testButton mt-5 md:mt-0" onClick={() => answer(4)}>answer 4</button>
+          </div>
+        </>
+      )}
     </>
   )
 }
