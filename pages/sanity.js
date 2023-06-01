8c180db27dff7caa409342cb128a6a9628705642
BIBruington <psychological_chemist@hotmail.com>
2023-05-31 21:46:19 -0400

    sanity config

--- pages/sanity.js
+++ pages/sanity.js
@@ -2,11 +2,20 @@ import {
   createImageUrlBuilder,
   createCurrentUserHook,
   createClient
- } from "next-sanity;"
+ } from "next-sanity";
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   apiVersion: "2021-03-25:",
   useCdn: process.env.NODE_ENV === "production",
-}
+};
+
+//fetches data in the getProps page functions
+export const sanityClient = createClient(config);
+
+//generates Image URLs with only asset reference data in your docs
+export const urlFor = (source) => createImageUrlBuilder(config).image(source);
+
+//function for using current logged in user account
+export const useCurrentUser = createCurrentUserHook(config);
