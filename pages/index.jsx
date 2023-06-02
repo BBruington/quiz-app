82db64a304ac5d3a61bd65f9b2a24f71bb2885ab
BIBruington <psychological_chemist@hotmail.com>
2023-06-02 00:12:08 -0400

    img background

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,25 @@
+import Link from "next/link";
 import { sanityClient, urlFor } from "./sanity";
 
 
 export default function Home({posts}) {
-  console.log(posts)
+  //console.log(posts)
   return (
     <>
       <div className="flex items-center justify-center mt-10">
         home page
+      </div>
+
+      <div>
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div>
+              {post.mainImage && (
+                <img src={urlFor(post.mainImage).url()} />
+              )}
+            </div>
+          </Link>
+        ))}
       </div>
     </>
   )
