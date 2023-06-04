9e1b2d3d819368d40b5e7265b015be9233025f43
BIBruington <psychological_chemist@hotmail.com>
2023-06-03 23:08:18 -0400

    posts page ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,16 +6,22 @@ export default function Home({posts}) {
   //console.log(posts)
   return (
     <>
-      <div className="flex items-center justify-center mt-10">
-        home page
-      </div>
-
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
         {posts.map((post) => (
           <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div>
+            <div className="">
               {post.mainImage && (
-                <img src={urlFor(post.mainImage).url()} />
+                <>
+                  <img src={urlFor(post.mainImage).url()} />
+                  <div className="flex justify-between p-5 bg-white">
+                    <div>
+                      <p>{post.title}</p>
+                      <p>{post.description} by {post.author.name}</p>
+                      <p></p>
+                    </div>
+                    <img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()} alt="" />
+                  </div>
+                </>
               )}
             </div>
           </Link>
