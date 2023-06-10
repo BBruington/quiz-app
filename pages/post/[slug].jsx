5f636d07af317dc5fb1e099ae8dbf850aa34ea81
BIBruington <psychological_chemist@hotmail.com>
2023-06-10 15:35:19 -0400

    create comment api

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -38,132 +38,132 @@ export default function Post({post}) {
           <h2 className="text-xl font-light text-gray-500 mb-2">{post.description}</h2>
           <div className="flex items-center space-x-2">
 
-<img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()} alt='' />
+          <img className="h-10 w-10 rounded-full" src={urlFor(post.author.image).url()} alt='' />
 
-<p className="font-extralight text-sm">
-  Blog post by <span className="text-green-600">{post.author.name}</span> - Published at{" "} 
-{new Date(post._createdAt).toLocaleString()}</p>
+          <p className="font-extralight text-sm">
+            Blog post by <span className="text-green-600">{post.author.name}</span> - Published at{" "} 
+          {new Date(post._createdAt).toLocaleString()}</p>
 
-</div>
+          </div>
 
-<div className="mt-10">
-<PortableText 
-  className=""
-  dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
-  projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-  content={post.body}
-  serializers={
-    {
-      h1: (props) => {
-        <h1 className="text-2xl font-bold my-5" {...props}/>
-      },
-      h2: (props) => {
-        <h1 className="text-xl font-bold my-5" {...props} />
-      },
-      li: ({ children }) => {
-        <li className="ml-4 list-disc">{children}</li>
-      },
-      link: ({ href, children }) => {
-        <a href={href} className="text-blue-500 hover:underline">
-          {children}
-        </a>
-      },
-    }
-  }
-/>
-</div>
-</article>
+          <div className="mt-10">
+          <PortableText 
+            className=""
+            dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+            projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+            content={post.body}
+            serializers={
+              {
+                h1: (props) => {
+                  <h1 className="text-2xl font-bold my-5" {...props}/>
+                },
+                h2: (props) => {
+                  <h1 className="text-xl font-bold my-5" {...props} />
+                },
+                li: ({ children }) => {
+                  <li className="ml-4 list-disc">{children}</li>
+                },
+                link: ({ href, children }) => {
+                  <a href={href} className="text-blue-500 hover:underline">
+                    {children}
+                  </a>
+                },
+              }
+            }
+          />
+          </div>
+        </article>
 
-<hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
+        <hr className="max-w-lg my-5 mx-auto border border-yellow-500" />
 
-{submitted ? (
-<div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
-  <h3 className="text-3xl font-bold">Thank you for your comment</h3>
-  <p>Once it has been approved, it will appear below.</p>
-</div>
-)
-:
-(
-<form 
-onSubmit={handleSubmit(onSubmit)}
-className="flex flex-col p-10 my-10 max-w-2xl mx-auto mb-10">
+        {submitted ? (
+        <div className="flex flex-col p-10 my-10 bg-yellow-500 text-white max-w-2xl mx-auto">
+          <h3 className="text-3xl font-bold">Thank you for your comment</h3>
+          <p>Once it has been approved, it will appear below.</p>
+        </div>
+        )
+        :
+        (
+        <form 
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col p-10 my-10 max-w-2xl mx-auto mb-10">
 
-<h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
-<h4 className="text-3xl font-bold">Leave a comment below!</h4>
-<hr className="py-3 my-2"></hr>
+        <h3 className="text-sm text-yellow-500">Enjoyed this article?</h3>
+        <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+        <hr className="py-3 my-2"></hr>
 
-<input 
-  {...register("_id")}
-  type="hidden"
-  name="_id"
-  value={post._id}
-/>
+        <input 
+          {...register("_id")}
+          type="hidden"
+          name="_id"
+          value={post._id}
+        />
 
-<label className="block mb-5">
-  <span className="text-gray-700">Name</span> {/*block elements have their own width */}
-  <input 
-  {...register("name", { required: true })}
-  className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
-  placeholder="John Smith" type="text" />
-</label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Name</span> {/*block elements have their own width */}
+          <input 
+          {...register("name", { required: true })}
+          className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+          placeholder="John Smith" type="text" />
+        </label>
 
-<label className="block mb-5">
-  <span className="text-gray-700">Email</span>
-  <input
-  {...register("email", { required: true })}
-  className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
-  placeholder="email@hotmail.com" type="email" />
-</label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Email</span>
+          <input
+          {...register("email", { required: true })}
+          className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+          placeholder="email@hotmail.com" type="email" />
+        </label>
 
-<label className="block mb-5">
-  <span className="text-gray-700">Comment</span>
-  <textarea 
-  {...register("comment", { required: true })}            
-  className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
-  placeholder="Leave a comment here" rows={8}/>
-</label>
+        <label className="block mb-5">
+          <span className="text-gray-700">Comment</span>
+          <textarea 
+          {...register("comment", { required: true })}            
+          className="shadow border rounded py-2 px-3 form-textarea mt-1 block w-full ring-yellow-500 outline-none focus:ring" 
+          placeholder="Leave a comment here" rows={8}/>
+        </label>
 
-{/* this is where errors from lack of fields validation appear */}
-<div className="flex flex-col p-5">
-  {/* this is connected to register / formstate and notices the fields are required */}
-  {errors.name && (
-    <span className="text-red-500">- The Name Field is required</span>
-  )}
-  {errors.comment && (
-    <span className="text-red-500">- The Comment Field is required</span>
-  )}
-  {errors.email && (
-    <span className="text-red-500">- The Email Field is required</span>
-  )}
-</div>
+        {/* this is where errors from lack of fields validation appear */}
+        <div className="flex flex-col p-5">
+          {/* this is connected to register / formstate and notices the fields are required */}
+          {errors.name && (
+            <span className="text-red-500">- The Name Field is required</span>
+          )}
+          {errors.comment && (
+            <span className="text-red-500">- The Comment Field is required</span>
+          )}
+          {errors.email && (
+            <span className="text-red-500">- The Email Field is required</span>
+          )}
+        </div>
 
-<input 
-type="submit" 
-className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer" 
-/>
+        <input 
+        type="submit" 
+        className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer" 
+        />
 
-</form>
+        </form>
 
-)}
+        )}
 
-{/* Comments are here */}
+        {/* Comments are here */}
 
-<div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
-<h3 className="text-4xl">Comments</h3>
-<hr className="pb-2" />
+        <div className="flex flex-col p-10 my-10 max-w-2xl mx-auto shadow-yellow-500 shadow space-y-2">
+        <h3 className="text-4xl">Comments</h3>
+        <hr className="pb-2" />
 
-{post.comments[0] ? (post.comments.map((comment) => (
-<div key={comment._id}>
-  <p>
-    <span className="text-yellow-500">{comment.name}: </span>{comment.comment}
-  </p>
-</div>
-)))
-:
-(
-<div>Wow such empty comments</div>
-)}
-</div>
+        {post.comments[0] ? (post.comments.map((comment) => (
+        <div key={comment._id}>
+          <p>
+            <span className="text-yellow-500">{comment.name}: </span>{comment.comment}
+          </p>
+        </div>
+        )))
+        :
+        (
+        <div>Wow such empty comments</div>
+        )}
+        </div>
       </main>
     </>
   )
