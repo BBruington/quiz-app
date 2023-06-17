02405a3387a9e4637c06973651ba090a126b35ef
BIBruington <psychological_chemist@hotmail.com>
2023-06-16 21:41:10 -0400

    create post api

---
+++ pages/api/createPost.js
@@ -0,0 +1,37 @@
+import {createClient} from "next-sanity";
+
+//required data for sanity to work
+const config = {
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  token: process.env.SANITY_API_TOKEN,
+}
+
+//config sanity client to mine
+const client = createClient(config);
+
+
+export default async function createComment(
+  req,
+  res
+) {
+  const {_id, title, description, author, email, mainImage, body, } = JSON.parse(req.body);
+
+  try{
+    await client.create({
+      _type: "post",
+      title,
+      description,
+      mainImage,
+      body,
+      author,
+      email,
+      comment
+    });
+  } catch(err) {
+    return res.status(500).json({ message: "Failed to submit comment", err});
+  }
+
+  console.log("Comment submitted");
+  res.status(200).json({ message: "Comment submitted" })
+}
