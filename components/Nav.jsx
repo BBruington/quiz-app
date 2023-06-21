754aa7329f204422226b84d2226710dd1bca9e3b
BIBruington <psychological_chemist@hotmail.com>
2023-06-21 18:39:43 -0400

    edit profile ui

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -12,6 +12,19 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 export default function Nav({userInfo}) {
   console.log(userInfo)
@@ -61,7 +74,39 @@ export default function Nav({userInfo}) {
         </SheetTrigger>
         <SheetContent>
           <SheetHeader>
-            <SheetTitle>Edit Profile</SheetTitle>
+            <SheetTitle>
+            <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+
+            </SheetTitle>
             <SheetDescription className="flex flex-col space-y-2 items-start">
               <button className="hover:underline" onClick={signOut}>
                 Sign Out
